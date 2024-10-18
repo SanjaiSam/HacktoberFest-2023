0957e78a42c47495e657bb3214661e56615b259b
SanjaiSam <samsanjai82@gmail.com>
2024-10-18 17:00:59 +0530

    Update insertalgo.py

--- insertalgo.py
+++ insertalgo.py
@@ -1,17 +1,24 @@
 def insertionSort(arr):
     n = len(arr)  
-      
+    
+    # If the array is empty or has one element, no need to sort
     if n <= 1:
         return   
- 
+    
+    # Traverse through 1 to n
     for i in range(1, n):  
-        key = arr[i] 
-        j = i-1
+        key = arr[i]  # The current element to be compared
+        j = i - 1
+        
+        # Move elements of arr[0..i-1] that are greater than key
+        # to one position ahead of their current position
         while j >= 0 and key < arr[j]:  
-            arr[j+1] = arr[j]  
+            arr[j + 1] = arr[j]  
             j -= 1
-        arr[j+1] = key  
-  
+            
+        arr[j + 1] = key  # Place the key in the correct position
+
+# Example usage
 arr = [12, 11, 13, 5, 6]
 insertionSort(arr)
-print(arr)
+print("Sorted array:", arr)
