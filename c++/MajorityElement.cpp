649405537ec041fadd916e360051c754e8b5960d
SanjaiSam <samsanjai82@gmail.com>
2024-10-19 07:22:28 +0530

    Update MajorityElement.cpp

--- c++/MajorityElement.cpp
+++ c++/MajorityElement.cpp
@@ -1,26 +1,40 @@
-class Solution
-{
+#include <vector>
+using namespace std;
+
+class Solution {
 public:
-    int majorityElement(vector<int> &nums)
-    {
+    int majorityElement(vector<int> &nums) {
         int cnt = 0;
-        int element;
+        int element = 0; // Initialize to 0 (or any value)
         int n = nums.size();
-        for (int i = 0; i < n; i++)
-        {
-            if (cnt == 0)
-            {
+
+        if (n == 0) {
+            throw invalid_argument("Input array is empty.");
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
                 element = nums[i];
             }
-            if (nums[i] == element)
-            {
+            if (nums[i] == element) {
                 cnt++;
-            }
-            else
-            {
+            } else {
                 cnt--;
             }
         }
-        return element;
+
+        // Optionally, verify that the found element is indeed the majority element
+        cnt = 0;
+        for (int num : nums) {
+            if (num == element) {
+                cnt++;
+            }
+        }
+
+        if (cnt > n / 2) {
+            return element;
+        } else {
+            throw invalid_argument("No majority element found.");
+        }
     }
 };
